ab0c58c83b44b87ea5cca8460ec588d5ac3cb3c5
Darren Torpey <darren.torpey@gmail.com>
2013-01-24 18:14:20 -0500

    Implemented the specified tQuery API

--- src/tiny-jquery.js
+++ src/tiny-jquery.js
@@ -1,1 +1,29 @@
-// Your implementation here.
+// tQuery
+// ------
+// A minimal jQuery-like library written for my on-site interview with Bocoup
+//  on January 24th, 2013 ~Darren Torpey
+(function(window, undefined) {
+  function tQuery(selector, context) {
+    if (!(this instanceof tQuery)) {
+      return new tQuery(selector, context);
+    }
+
+    context || (context = window.document)
+
+    this.results = context.querySelectorAll(selector);
+    for (var i = 0; i < this.results.length; ++i) {
+      this[i] = this.results[i];
+    }
+    this.length = this.results.length;
+
+    return this;
+  }
+  tQuery.prototype.forEach = function(callback) {
+    for (var i = 0; i < this.results.length; ++i) {
+      var result = this.results[i];
+      callback.apply(result, [result, i]);
+    }
+  };
+
+  window.tQuery = window.$$ = tQuery;
+})(window);
